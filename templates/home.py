9537bc09bdfb7a27ac68a79cc86cf2ddce33d5a4
HassanSherwani <45789548+HassanSherwani@users.noreply.github.com>
2019-04-14 12:51:14 +0200

    adding home file with initial code
    This file will render a text form where a user can enter a message

---
+++ templates/home.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template,url_for,request
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+        return "hello world, this is my baby step "
+
+if __name__ == '__main__':
+	app.run(debug=True)
